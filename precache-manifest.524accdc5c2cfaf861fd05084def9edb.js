a30ea146fef07cd96e1679f7783cb5f6a1d7d92b
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-04-28 09:39:27 -0300

    Updates

---
+++ precache-manifest.524accdc5c2cfaf861fd05084def9edb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d30ced94c424745601b30891817848f",
+    "url": "/mathjax3-react/index.html"
+  },
+  {
+    "revision": "c2d67961b1a706221c73",
+    "url": "/mathjax3-react/static/js/2.685ee15c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mathjax3-react/static/js/2.685ee15c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d3c5f8e3f653e88e304",
+    "url": "/mathjax3-react/static/js/main.e96dc9b1.chunk.js"
+  },
+  {
+    "revision": "71badfb4869fd5d01631",
+    "url": "/mathjax3-react/static/js/runtime-main.2f1081e8.js"
+  }
+]);
