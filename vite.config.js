64cd3a168964829ce912027f8551e2c039f7b96a
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2023-11-24 13:17:37 -0300

    chore: add vite and remove babel

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "mathjax3-react",
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          reactDOM: "ReactDOM",
+        },
+      },
+    },
+  },
+});
